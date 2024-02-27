2350d7c55db5cf772210b043604ea85d1bd1d855
Lucas Spitzer <104842840+pangolino30@users.noreply.github.com>
2024-02-27 15:11:26 +0100

    ajout shadow card

--- index.php
+++ index.php
@@ -34,34 +34,34 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <a data-bs-toggle="modal" data-bs-target="#decouvrirModal"><img class="card-img rounded-5" src="/assets/img/decouvrir.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <a data-bs-toggle="modal" data-bs-target="#actualiteModal"><img class="card-img rounded-5" src="/assets/img/actualites.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <a href="/assets/Quizz-IA-Table-Rondes-CCI-GARD.pdf" target="_blank"><img class="card-img rounded-5" src="/assets/img/quizz.png"></a>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <a data-bs-toggle="modal" data-bs-target="#listeiaModal"><img class="card-img rounded-5" src="/assets/img/listeia.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <a data-bs-toggle="modal" data-bs-target="#dangerModal"><img class="card-img rounded-5" src="/assets/img/danger.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <div class="card-body">
                             <audio controls src="/assets/audio/capsule-deepfake.mp3"></audio>
                             <audio controls src="/assets/audio/capsule-soltani-nolwen.mp3"></audio>
@@ -69,17 +69,17 @@
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <a data-bs-toggle="modal" data-bs-target="#refModal"><img class="card-img rounded-5" src="/assets/img/ref.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <a data-bs-toggle="modal" data-bs-target="#remplacementModal"><img class="card-img rounded-5" src="/assets/img/remplacement.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
-                    <div class="card clickable rounded-5">
+                    <div class="card clickable rounded-5 shadow">
                         <a data-bs-toggle="modal" data-bs-target="#videoModal"><img class="card-img rounded-5" src="/assets/img/kit-video.png"></a>
                     </div>
                 </div>
