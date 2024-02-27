7b10a9612e545cd97f1fb8b53d76582874773993
adammarzuk <adam.marzuk@lycee-cci-gard.fr>
2024-02-27 11:43:33 +0100

    Ajouts des modals demandé par la prof

---
+++ assets/modals/video.php
@@ -0,0 +1,14 @@
+<div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="VideoModalLabel" aria-hidden="true">
+    <div class="modal-dialog m-lg-5 m-md-3 m-sm-1">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="VideoModalLabel">Kit Vidéo sur les IA</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body container">
+                <div class="row">
+                    <iframe src="" frameborder="0"></iframe><!--ICI LA VIDEO DE JEANETTE-->
+                </div>
+            </div>
+        </div>
+    </div>
