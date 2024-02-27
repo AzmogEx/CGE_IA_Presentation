6469214db652171bcf6d478ad140605899dc2584
Aldwin <elonyx7@gmail.com>
2024-02-27 13:03:54 +0100

    add logo cci and metropole

--- index.php
+++ index.php
@@ -14,38 +14,49 @@
     <!--NAVBAR-->
     <header></header>
     <!-- TITRE -->
-    <div class="container mt-5" align="center">
-        <h1 class="display-6 align-center archivo-black-regular">Kit de prévention sur les IA</h1>
+
+    <div class="container mt-5">
+        <div class="logo-cci-top"><img class="logo-cci" src="./assets/img/logo_cci_gard_web.png" alt="logo-cci"></div>
+        <h1 align="center" class="display-6 align-center archivo-black-regular">Kit de prévention sur les IA</h1>
+        <div class="logo-metropole-top"><img class="logo-metropole" src="./assets/img/métropole.png"
+                alt="logo-métropole">
+        </div>
     </div>
+
     <!-- CARTES BENTO -->
     <section>
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                     <div class="card clickable rounded-5">
-                        <a data-bs-toggle="modal" data-bs-target="#decouvrirModal"><img class="card-img rounded-5" src="/assets/img/decouvrir.png"></a>
+                        <a data-bs-toggle="modal" data-bs-target="#decouvrirModal"><img class="card-img rounded-5"
+                                src="/assets/img/decouvrir.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                     <div class="card clickable rounded-5">
-                        <a data-bs-toggle="modal" data-bs-target="#actualiteModal"><img class="card-img rounded-5" src="/assets/img/actualites.png"></a>
+                        <a data-bs-toggle="modal" data-bs-target="#actualiteModal"><img class="card-img rounded-5"
+                                src="/assets/img/actualites.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                     <div class="card clickable rounded-5">
-                        <a href="/assets/Quizz-IA-Table-Rondes-CCI-GARD.pdf" target="_blank"><img class="card-img rounded-5" src="/assets/img/quizz.png"></a>
+                        <a href="/assets/Quizz-IA-Table-Rondes-CCI-GARD.pdf" target="_blank"><img
+                                class="card-img rounded-5" src="/assets/img/quizz.png"></a>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                     <div class="card clickable rounded-5">
-                        <a data-bs-toggle="modal" data-bs-target="#listeiaModal"><img class="card-img rounded-5" src="/assets/img/listeia.png"></a>
+                        <a data-bs-toggle="modal" data-bs-target="#listeiaModal"><img class="card-img rounded-5"
+                                src="/assets/img/listeia.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                     <div class="card clickable rounded-5">
-                        <a data-bs-toggle="modal" data-bs-target="#dangerModal"><img class="card-img rounded-5" src="/assets/img/danger.png"></a>
+                        <a data-bs-toggle="modal" data-bs-target="#dangerModal"><img class="card-img rounded-5"
+                                src="/assets/img/danger.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
@@ -58,12 +69,14 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                     <div class="card clickable rounded-5">
-                        <a data-bs-toggle="modal" data-bs-target="#refModal"><img class="card-img rounded-5" src="/assets/img/ref.png"></a>
+                        <a data-bs-toggle="modal" data-bs-target="#refModal"><img class="card-img rounded-5"
+                                src="/assets/img/ref.png"></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                     <div class="card clickable rounded-5">
-                        <a data-bs-toggle="modal" data-bs-target="#remplacementModal"><img class="card-img rounded-5" src="/assets/img/remplacement.png"></a>
+                        <a data-bs-toggle="modal" data-bs-target="#remplacementModal"><img class="card-img rounded-5"
+                                src="/assets/img/remplacement.png"></a>
                     </div>
                 </div>
             </div>
@@ -72,7 +85,8 @@
 
     <footer class="container d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <p class="col-md-4 mb-0 text-body-secondary">© 2024 CCI GARD NIMES</p>
-        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <a href="/"
+            class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <img class="bi me-2" src="/assets/img/logo_cci_gard_web.png" height="50" alt="" />
         </a>
         <ul class="nav col-md-4 justify-content-end">
@@ -80,11 +94,14 @@
             <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Carte</a></li>
             <li class="nav-item"><a href="/faq.html" class="nav-link px-2 text-body-secondary">FAQ</a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">A propos</a></li>
-            <li class="nav-item"><a href="/partenaires.php" class="nav-link px-2 text-body-secondary">Partenaires</a></li>
+            <li class="nav-item"><a href="/partenaires.php" class="nav-link px-2 text-body-secondary">Partenaires</a>
+            </li>
         </ul>
     </footer>
     <script src="/js/nav.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
 
 <!-- MODAL DECOUVRIR -->
