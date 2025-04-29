295dd1492ef7f70cf92adefef84232e2e90d5b96
Lucas Spitzer <104842840+spitzerl@users.noreply.github.com>
2025-04-29 18:26:03 +0200

    Revert "Fully recreating project"

---
+++ assets/modals/actualite.php
@@ -0,0 +1,78 @@
+<div class="modal fade" id="actualiteModal" tabindex="-1" aria-labelledby="actualiteModalLabel" aria-hidden="true">
+    <div class="modal-dialog m-lg-5 m-md-3 m-sm-1">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="actualiteModalLabel">Actualités</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body container">
+                <div class="row">
+                    <div class="col d-flex align-items-stretch">
+                        <div class="card mb-2">
+                            <img src="https://www.leparisien.fr/resizer/k_klETO9sa87i65eU9c5J6hdY6I=/932x582/cloudfront-eu-central-1.images.arcpublishing.com/leparisien/ZVGEJXATVJCNBKUQ3TR2AU4IIY.jpg" class="card-img" alt="Photo d'un affiche sur le deep fake">
+                            <div class="card-body">
+                                <a href="https://www.leparisien.fr/societe/cest-quoi-mia-loutil-dintelligence-artificielle-bientot-deploye-pour-tous-les-eleves-de-seconde-06-12-2023-UP25SKQEMRBOXGEXGAIWBVJQ5I.php">
+                                    <h5 class="card-title">C’est quoi MIA, l’outil d’intelligence artificielle bientôt déployé pour tous les élèves de seconde ?</h5>
+                                </a>
+                                <p class="card-text">Découvrez MIA, la nouvelle intelligence artificielle du gouvernement pour aider les lycéens dans leur apprentissage.</p>
+                            </div>
+                            <div class="card-footer bg-body-tertiary border-0">
+                                <small class="text-body-secondary">Le Parisien</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col d-flex align-items-stretch">
+                        <div class="card mb-2">
+                            <img src="https://www.01net.com/app/uploads/2024/01/deepfake-marine-le-pen-2-896x596.jpg" class="card-img" alt="Photo d'un affiche sur le deep fake">
+                            <div class="card-body">
+                                <a href="https://www.01net.com/actualites/marine-le-pen-en-russe-le-deepfake-qui-brouille-le-message-du-gouvernement.html">
+                                    <h5 class="card-title">Marine Le Pen en russe</h5>
+                                </a>
+                                <p class="card-text">Alors que le gouvernement veut encadrer de près les pratiques de trucages numériques dans les vidéos — les deepfakes —, un porte-parole du parti présidentiel a mis en ligne une vidéo deepfake de Marine Le Pen.</p>
+                            </div>
+                            <div class="card-footer bg-body-tertiary border-0">
+                                <small class="text-body-secondary">01 NET</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col d-flex align-items-stretch">
+                        <div class="card mb-2">
+                            <img src="https://www.digitaltrends.com/wp-content/uploads/2024/01/DT-CES-2024-Feature-Image-Topic.jpg?resize=1200%2C630&p=1" class="card-img" alt="Infographie CES 2024">
+                            <div class="card-body">
+                                <a href="https://www.bfmtv.com/tech/nouveautes-produits/direct-ces-2024-sommeil-handicap-les-innovations-tech-ciblent-le-bien-etre-et-la-sante_LN-202401110095.html">
+                                    <h5 class="card-title">CES 2024: jumelles identifiant les oiseaux, IA avec un visage... Tous les produits présentés</h5>
+                                </a>
+                                <p class="card-text">Le CES 2024 entame sa troisième journée. Le salon de Las Vegas continue de dévoiler les innovations qui feront le quotidien de demain, notamment dans le domaine de la santé, du bien-être et de l'aide à la personne.</p>
+                            </div>
+                            <div class="card-footer bg-body-tertiary border-0">
+                                <small class="text-body-secondary">BFMTV Tech&Co</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col d-flex align-items-stretch">
+                        <div class="card mb-2">
+                            <img src="https://static-images.lpnt.fr/cd-cw1618/images/2024/01/17/25917989lpw-25918083-mega-une-jpg_10053918.jpg" class="card-img" alt="Doigt pointé sur un holograme du corps humain">
+                            <div class="card-body">
+                                <a href="https://www.lepoint.fr/sante/l-ia-de-google-plus-forte-que-les-medecins-17-01-2024-2550040_40.php">
+                                    <h5 class="card-title">L’IA de Google plus forte que les médecins</h5>
+                                </a>
+                                <p class="card-text">Une nouvelle étude menée par la firme californienne montre qu’une intelligence artificielle type ChatGPT fait un meilleur diagnostic qu’un véritable médecin.</p>
+                            </div>
+                            <div class="card-footer bg-body-tertiary border-0">
+                                <small class="text-body-secondary">Le Point</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mx-1">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Plus d'informations sur Google Actualités</h4>
+                            <a class="btn btn-cci" href="https://news.google.com/topics/CAAqIAgKIhpDQkFTRFFvSEwyMHZNRzFyZWhJQ1puSW9BQVAB?ceid=FR:fr&oc=3" role="button"><i class="bi bi-box-arrow-up-right"></i> Accéder</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
